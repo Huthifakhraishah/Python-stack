224d4c377154522894c85778b1c3b38be31771fa
huthifa <huthifakhraishah98@gmail.com>
2021-05-05 12:54:56 +0300

    First commit

---
+++ python/pyhton-fundementals/hello_world.py
@@ -0,0 +1,12 @@
+print("hello world")
+name="huthifa khraishah"
+print("Hello",name)
+print("Hello" + name)
+num =11
+print("hello",num)
+print(f"hello {num}")
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print( "I love to eat {} and {}".format(fave_food1,fave_food2)) 
+print( f"I love to eat {fave_food1} and {fave_food2}" )
+
