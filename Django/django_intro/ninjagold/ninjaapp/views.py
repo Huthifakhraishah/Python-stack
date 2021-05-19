437515cb1f4eb31cbf3835e517c17eee19bfee6f
huthifa <huthifakhraishah98@gmail.com>
2021-05-20 00:43:02 +0300

    update

---
+++ Django/django_intro/ninjagold/ninjaapp/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import redirect, render
+from time import gmtime, strftime
+import random
+def index(request):
+    if request.session['geust'] == False:
+        request.session['count']=1
+        request.session["geust"]=0
+        return render(request,'index.html')
+    else:
+        return render(request,'index.html')
+def process(request):
+    if request.POST['process'] == 'process1':
+        request.session['count']+=1
+        request.session['earn']=random.randint(10,20)
+        request.session["geust"]+=request.session['earn']
+        return redirect('/')
+    elif request.POST['process'] == 'process2':
+        request.session['count']+=1
+        request.session['earn']= random.randint(5,10)
+        request.session["geust"]+=request.session['earn']
+        return redirect('/')
+    elif request.POST['process'] == 'process3':
+        request.session['count']+=1
+        request.session['earn']=random.randint(2,5)
+        request.session["geust"]+=request.session['earn']
+        return redirect('/')
+    elif request.POST['process'] == 'process4':
+        request.session['count']+=1
+        request.session['earn']=random.randint(-50,50)
+        request.session["geust"]+=request.session['earn']
+        return redirect('/')
+    else:
+        return redirect('/')
